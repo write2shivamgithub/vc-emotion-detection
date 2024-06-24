e6191dba2088226205ac0c2dd61d0a6d4cebc89d
write2shivamgithub <shivamshankaraacharya@gmail.com>
2024-06-24 12:58:38 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A ML project to predict emotions from text.',
+    author='Shankaraacharya',
+    license='',
+)
